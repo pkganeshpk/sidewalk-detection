b039f03728995e8d991d9cd6efdde8becedbf90e
Ganesh <pkganeshpk@gmail.com>
2016-02-10 15:20:09 -0700

    organized catkin workspace

--- src/detect_node.py
+++ src/detect_node.py
@@ -1,11 +1,13 @@
 import rosbag
 import cv2
-from cv_bridge import CvBridge, CvBridgeError
+
 
 bag = rosbag.Bag('../realsense.bag', 'r')
 
 # Get list of topics
 for topic, msg, t in bag.read_messages(topics = ['/camera/color/image_raw']):
-   cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+   print "!"	
+   clrimg = CvBridge.bridge.imgmsg_to_cv2(data, "bgr8")
+   cv2.imshow('image',clrimg)	
 	
 bag.close()
