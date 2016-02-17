e0385bec84ef0a79583a24d39b2b786105b36c9d
Ganesh <pkganeshpk@gmail.com>
2016-02-16 18:29:30 -0700

    Changed code

--- src/detect_node.py
+++ src/detect_node.py
@@ -3,7 +3,7 @@ import cv2
 from image_converter import image_converter
 import rospy
 
-I = image_converter()
+I = image_converter('images/frame0000.jpg')
 rospy.spin()
 
 
--- src/image_converter.py
+++ src/image_converter.py
@@ -15,7 +15,7 @@ class image_converter:
 
   sidewalk_colors = [ [90, 105] , [90, 105], [90, 105]]
 
-  def __init__(self):
+  def __init__(self, ref_img_path):
     print 'Image_converter'	
     rospy.init_node('sidewalk_detector', anonymous = True)
     self.bridge = CvBridge()
@@ -28,41 +28,38 @@ class image_converter:
     self.depth_out_pub = rospy.Publisher("/depth/points_out",Image, queue_size = 1)
 
     self.imgcount = 0
-    self.p_thresh = 0.8 # Threshold probability for 		
+    self.p_thresh = 0.9 # Threshold probability for 
+    self.ref_img = cv2.imread(ref_img_path)			
+    print 'Shape of ref_img = ', self.ref_img.shape
     	
+    self.get_hist_sw()
+    self.get_hist_bg()
+    print 'size of histograms = ', self.hist_sw.shape
+    assert(self.hist_bg.shape == self.hist_sw.shape)		
+    
+    #cv2.imshow('red', self.ref_img[50:200,200:450])
+    #cv2.waitKey(10)	 
+    print 'correl = ', cv2.compareHist(self.hist_sw, self.hist_bg, cv2.cv.CV_COMP_CORREL)
+
 
   def color_callback(self,data):
-    	
     try:
       self.color_img = self.bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
       print ("Exception:", e)
   
-    if(self.imgcount == 0):
-       self.imgcount = 1 	 
-       # Get training histogram & background histogram
-       self.get_hist_sw()
-       self.get_hist_bg()
-       print 'size of histograms = ', self.hist_sw.shape
-       assert(self.hist_bg.shape == self.hist_sw.shape)		
-    
-       cv2.imshow('background', self.bg_img)
-       #cv2.waitKey(10)	 
-       print 'correl = ', cv2.compareHist(self.hist_sw, self.hist_bg, cv2.cv.CV_COMP_CORREL)
-     
-    else:
-	pass
-	self.color_new_img()
-        cv2.imshow('red image', self.red_img)
-	cv2.waitKey(1)
+  
+	
+    self.color_new_img()
+    cv2.imshow('red image', self.red_img)
+    cv2.waitKey(10)
 	
     	
   def color_new_img(self):
-     print "!"
      self.hsv_cur = cv2.cvtColor(self.color_img, cv2.COLOR_BGR2HSV)
      rows = self.hsv_cur.shape[0]
      cols = self.hsv_cur.shape[1]
-     #assert(self.hist_sw.shape == self.hsv_cur.shape)
+     # assert(self.hist_sw.shape == self.hsv_cur.shape)
 
 
      self.red_img = self.color_img[:]	
@@ -83,14 +80,21 @@ class image_converter:
 
   def get_hist_bg(self):
     
-     self.bg_img = self.color_img[200:300,100:200]
+     self.bg_img = self.ref_img[50:200,200:450]
      height = self.bg_img.shape[0]
      width = self.bg_img.shape[1]	
      for i in xrange(height):
 	for j in xrange(width):
-		row = np.random.rand() * self.color_img.shape[0] 				     
-		col = np.random.rand() * self.color_img.shape[1]
-                self.bg_img[i][j] =  self.color_img[row][col] 
+		
+		while (True):
+			row = int(np.random.rand() * self.ref_img.shape[0]) 				     
+			col = int(np.random.rand() * self.ref_img.shape[1])
+			if (50 <= row <=200 and 200<= col <=450):
+				continue
+			else:
+				break
+			
+                self.bg_img[i][j] =  self.ref_img[row][col] 
      
       		
      self.hsv_bg = cv2.cvtColor(self.bg_img, cv2.COLOR_BGR2HSV)
@@ -99,7 +103,7 @@ class image_converter:
      ''' Didn't normalize '''
 
   def get_hist_sw(self):
-    self.hsv_sw = cv2.cvtColor(self.color_img[50:200,100:200], cv2.COLOR_BGR2HSV)
+    self.hsv_sw = cv2.cvtColor(self.ref_img[50:200,200:450], cv2.COLOR_BGR2HSV)
     self.hist_sw = cv2.calcHist([self.hsv_sw], [0, 1], None, [180, 256], [0, 180, 0, 256])
     print 'sidewalk histogram:', self.hist_sw.shape
     print 'sidewalk hsv:', self.hsv_sw.shape	
